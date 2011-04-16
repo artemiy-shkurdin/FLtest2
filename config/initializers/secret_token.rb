9c042e544facddfea9936fafb6dc9b480b7e4fd9
bitter <bitter@bitter-NB.(none)>
2011-04-16 09:55:27 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flattest::Application.config.secret_token = '9b96633d2414485bcd47941acb8d8380015b09b78e34607342af15a2c54546911ae8cfe136be2a296c2a5311f553fcfa469ebb799058ec36a9201b737e86ba23'
